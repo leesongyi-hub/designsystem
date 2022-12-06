49b244e845ebf17040a74a34bbf26b4ad7683bc0
lee-song-yi <okpo5071@gmail.com>
2022-12-06 10:03:59 +0900

    test

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,17 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Fri, 15 Jul 2022 19:49:49 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const TypographyBodyH3FontWeight;
+extern float const TypographyBodyH4StrikeThroughFontWeight;
+extern float const TypographyBodyItalicFontWeight;
+extern float const TypographyBodyExtraBoldCondensedItalicFontWeight;
+extern float const TypographyBodyMediumExtendedItalicFontWeight;
+extern float const TypographyBodySuperFontWeight;

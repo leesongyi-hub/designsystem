5d91893575371a9f11c257b7022651d7745760ad
lee-song-yi <okpo5071@gmail.com>
2022-12-06 13:33:06 +0900

    환경셋팅

---
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -0,0 +1,69 @@
+
+//
+// StyleDictionary+Struct.swift
+//
+
+// Do not edit directly
+// Generated on Tue, 06 Dec 2022 04:30:50 GMT
+
+
+import UIKit
+
+internal struct StyleDictionaryStruct {
+    internal static let backgroundDark = #eaebec
+    internal static let backgroundDefault = #f5f5f5
+    internal static let backgroundLight = #f9fafa
+    internal static let backgroundPrimaryDark = #c7ecff
+    internal static let backgroundPrimaryDefault = #dcf3ff
+    internal static let backgroundPrimaryLight = #f5faff
+    internal static let black = #000000
+    internal static let blue100 = #dcf3ff
+    internal static let blue200 = #c7ecff
+    internal static let blue300 = #8fd8ff
+    internal static let blue400 = #66c7ff
+    internal static let blue50 = #f5faff
+    internal static let blue500 = #31a8f6
+    internal static let blue600 = #2487eb
+    internal static let blue700 = #1674df
+    internal static let blue800 = #1061cb
+    internal static let blue900 = #0e49af
+    internal static let borderDark = #989c9f
+    internal static let borderDefault = #dddedf
+    internal static let borderLight = #eaebec
+    internal static let borderPrimary = #31a8f6
+    internal static let colorBaseGrayDark = UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha: 1)
+    internal static let colorBaseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    internal static let colorBaseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
+    internal static let colorBaseGreen = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    internal static let colorBaseRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    internal static let colorFontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    internal static let colorFontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    internal static let colorFontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    internal static let disabled = #dddedf
+    internal static let font100 = "#111213"
+    internal static let font300 = "#323639"
+    internal static let font700 = "#757b7f"
+    internal static let font900 = "#989c9f"
+    internal static let fontLink = "#1061cb"
+    internal static let fontPrimary = "#2487eb"
+    internal static let gray100 = #f9fafa
+    internal static let gray200 = #f5f5f5
+    internal static let gray300 = #eaebec
+    internal static let gray400 = #dddedf
+    internal static let gray500 = #989c9f
+    internal static let gray600 = #757b7f
+    internal static let gray700 = #535a60
+    internal static let gray800 = #323639
+    internal static let gray900 = #111213
+    internal static let iconBlack = #323639
+    internal static let iconGray = #757b7f
+    internal static let iconPrimary = #2487eb
+    internal static let primaryTestTest = #2487eb
+    internal static let primaryTestTest333 = #2487eb
+    internal static let sizeFontBase = CGFloat(16.00) /* the base size of the font */
+    internal static let sizeFontLarge = CGFloat(32.00) /* the large size of the font */
+    internal static let sizeFontMedium = CGFloat(16.00) /* the medium size of the font */
+    internal static let sizeFontSmall = CGFloat(12.00) /* the small size of the font */
+    internal static let tokenSetOrder0 = global
+    internal static let white = #ffffff
+}
